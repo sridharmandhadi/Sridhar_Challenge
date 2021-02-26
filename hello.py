8179f01c3248dd57eb40d5736c0d6bd886e8f9b1
sridharmandhadi <sridharmandhadi@gmail.com>
2021-02-26 17:18:27 -0500

    Updated Python coding challenge code file

---
+++ hello.py
@@ -0,0 +1,23 @@
+from itertools import groupby
+import re
+
+N=int(input())
+entr = [ input()  for i in range(N) ]
+
+
+def count_consecutive(i):
+     return max([len(list(g)) for k, g in groupby(i)])
+
+for i in entr:
+    pattn = re.compile(r"^[4,5,6]([0-9]{15}$)")
+    pattn2 = re.compile(r"^[4,5,6]\d{3}-[0-9]{4}-[0-9]{4}-[0-9]{4}$")
+
+
+    if re.search(pattn, i) or re.search(pattn2, i) :
+        if count_consecutive(i.replace('-',""))>=4 :
+           print('Invalid')
+        else:
+           print('Valid')
+    else:
+         print("Invalid")
+
